b8ac89e3fe111f886a014547348b5a0764b98af9
Kseniya <grickovaKS@gmail.com>
2019-10-06 23:02:41 +0300

    add constructors

--- General_CD_Collection/heder.h
+++ General_CD_Collection/heder.h
@@ -3,12 +3,37 @@ using namespace std;
 class CDcollection
 {
     public:
-      CDcollection(int nPerson, int nCD);
-      ~CDcollection();
-      void add(char *CDname, int idPerson); // добавить новый диск.
-      void transfer(char *CDname, int idParent, int idChild); // дать дсик idPerson.
-      int where (char *CDname); // У кого диск.
-      int amount(int idPerson); // сколько дисков у idPerson.
-      char *nameCD(int idPerson, int i); // название i-того диска у idPerson.
-      void print(); // распечатать состояние объекта.
+        /// Хранилище двух массивов: владельцев и колекции дисков.
+        CDcollection(int nPerson, int nCD);
+
+        /// Деструктор.
+        ~CDcollection();
+
+        /// Добавить новый диск.
+        void add(char *CDname, int idPerson);
+
+        /// Дать дсик idPerson.
+        void transfer(char *CDname, int idParent, int idChild); 
+
+        /// У кого находится диск.
+        int where (char *CDname); 
+
+        /// Сколько дисков у idPerson.
+        int amount(int idPerson); 
+
+        /// Название i-того диска у idPerson.
+        char *nameCD(int idPerson, int i); 
+
+        /// Распечатать состояние объекта.
+        void print(); 
+    
+    private:
+        int nPerson;
+        int nCD;
+        char CDname;
+        int isPerson;
+        int idParent;
+        int idChild;
+        int i;
+
 };
