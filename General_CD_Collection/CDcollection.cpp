5f588d639f49dc570a4d3b6c442fddd8a2475571
Kseniya <grickovaKS@gmail.com>
2019-10-06 23:13:09 +0300

    add class constructors

---
+++ General_CD_Collection/CDcollection.cpp
@@ -0,0 +1,30 @@
+#include "heder.h"
+
+CDcollection::CDcollection(int nPerson, int nCD){
+
+
+}
+
+void CDcollection::add(char *CDname, int idPerson){
+
+}
+
+void CDcollection::transfer(char *CDname, int idParent, int idChild){
+
+}
+
+int CDcollection::where (char *CDname){
+
+}
+
+int CDcollection::amount(int idPerson){
+
+}
+
+char CDcollection::*nameCD(int idPerson, int i){
+
+}
+
+void CDcollection::print(){
+    
+}
