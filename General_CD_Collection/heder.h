1b14e5ade17fadc2c4b283247d355662f172a950
Kseniya <grickovaKS@gmail.com>
2019-10-06 22:39:15 +0300

    first commit

---
+++ General_CD_Collection/heder.h
@@ -0,0 +1,12 @@
+class CDcollection
+{
+    public:
+      CDcollection(int nPerson, int nCD);
+      ~CDcollection();
+      void add(char *CDname, int idPerson); // добавить новый диск.
+      void transfer(char *CDname, int idParent, int idChild); // дать дсик idPerson.
+      int where (char *CDname); // У кого диск.
+      int amount(int idPerson); // сколько дисков у idPerson.
+      char *nameCD(int idPerson, int i); // название i-того диска у idPerson.
+      void print(); // распечатать состояние объекта.
+};
